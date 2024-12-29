b12d9a9be42ea48a302f00586ad44facbd6c489a
Michal Louda <michal.louda@chargedmonkey.com>
2024-12-29 17:13:53 +0100

    auto dim component

--- Assets/Scripts/Core/Modules/Ui/Components/AutoDimComponent.cs
+++ Assets/Scripts/Core/Modules/Ui/Components/AutoDimComponent.cs
@@ -1,7 +1,50 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
 namespace OneDay.Core.Modules.Ui.Components
 {
-    public class AutoDimComponent
+    [RequireComponent(typeof(CanvasGroup))]
+    public class AutoDimComponent : MonoBehaviour, IPointerDownHandler
     {
-        
+        [SerializeField] private CanvasGroup canvasGroup;
+        [SerializeField] private float dimedAlpha = 0.1f;
+        [SerializeField] private float visibleTime = 3.0f;
+        [SerializeField] private float fadeSpeed = 2f;
+     
+        private float visibilityTimer;
+        private void OnEnable()
+        {
+            visibilityTimer = visibleTime;    
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            visibleTime = visibilityTimer;
+        }
+
+        private void Update()
+        {
+            if (visibilityTimer > 0)
+            {
+                if (canvasGroup.alpha < 1)
+                {
+                    float alphaStep = fadeSpeed * Time.deltaTime;
+                    float alpha = canvasGroup.alpha;
+                    alpha += alphaStep;
+                    canvasGroup.alpha = Mathf.Clamp01(alpha);
+                }
+                visibilityTimer -= Time.deltaTime;
+            }
+            else
+            {
+                if (canvasGroup.alpha > dimedAlpha)
+                {
+                    float alphaStep = fadeSpeed * Time.deltaTime;
+                    float alpha = canvasGroup.alpha;
+                    alpha -= alphaStep;
+                    canvasGroup.alpha = Mathf.Clamp01(alpha);
+                }
+            }
+        }
     }
 }
